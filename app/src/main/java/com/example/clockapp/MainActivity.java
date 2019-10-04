e7c6ec9a6a5a38c7befaa70cd9d90ea539029974
z5215512 <z5215512@ad.unsw.edu.au>
2019-10-04 18:46:52 +1000

    update
    update

--- app/src/main/java/com/example/clockapp/MainActivity.java
+++ app/src/main/java/com/example/clockapp/MainActivity.java
@@ -4,23 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.sql.Time;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+    public static boolean twelveHr = true;
+    public static String time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Time syd = new Time (Calendar.HOUR,Calendar.MINUTE,Calendar.SECOND);
         //sydney clock
         ConstraintLayout sydney = findViewById(R.id.sydney);
         ImageView sydneyIcon = sydney.findViewById(R.id.cityImage);
         sydneyIcon.setImageResource(R.drawable.sydney);
         TextView sydneyText = sydney.findViewById(R.id.name);
         sydneyText.setText("Sydney");
-        TextView sydneyTime = sydney.findViewById(R.id.time);
-        sydneyTime.setText("00:00");
+        final TextView sydneyTime = sydney.findViewById(R.id.time);
+        time = String.format(syd.toString().substring(0, 5));
+        sydneyTime.setText(time);
+
         //moscow clock
         ConstraintLayout moscow = findViewById(R.id.moscow);
         ImageView moscowIcon = moscow.findViewById(R.id.cityImage);
@@ -61,5 +71,33 @@ public class MainActivity extends AppCompatActivity {
         tokyoText.setText("Tokyo");
         TextView tokyoTime = tokyo.findViewById(R.id.time);
         tokyoTime.setText("00:00");
+
+        //switching time format
+        Button switcher = findViewById(R.id.timeFormat);
+        switcher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String temp = formatTime(time);
+                time = temp;
+                sydneyTime.setText(time);
+            }
+        });
     }
+
+    //couldn't figure out how to get and switch the time :(
+    public static String formatTime(String time){
+        if(twelveHr){
+            Time syd = new Time (Calendar.HOUR_OF_DAY,Calendar.MINUTE,Calendar.SECOND);
+            String a = syd.toString().substring(0,5);
+            twelveHr = false;
+            return a;
+        }
+        else {
+            Time syd = new Time (Calendar.HOUR,Calendar.MINUTE,Calendar.SECOND);
+            String a = syd.toString().substring(0,5);
+            twelveHr = true;
+            return a;
+        }
+    }
+
 }
